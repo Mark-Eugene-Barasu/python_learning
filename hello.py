a5d8214e7c6f05cf90ec0ea6598011187699203d
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-02 05:47:02 +0200

    corrected the number to string concatination syntax

--- hello.py
+++ hello.py
@@ -4,5 +4,7 @@ b = 3.14
 print(a)  # Output: 10
 print(b)  # Output: 3.14
 
-print(a + "is my number")  # This will raise a TypeError because you cannot concatenate a string and an integer directly
+# print(a + "is my number")
+# This will raise a TypeError because you cannot concatenate a string and an integer directly
 # To fix this, you can convert the integer to a string:
+print(str(a) + " is my number")  # Output: "10 is my number"
