e1aee2f6f389bac4a7e50b2984d419b88e711563
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-02 05:58:08 +0200

    getting user input

--- hello.py
+++ hello.py
@@ -1,19 +1,4 @@
-from math import *
-
-# number methods in Python
-a = 10
-print(abs(a))  # absolute value
-print(pow(a, 2))  # power
-print(round(3.14159, 2))  # round to 2 decimal places
-print(max(1, 5, 3))  # maximum value
-print(min(1, 5, 3))  # minimum value
-print(sum([1, 2, 3, 4, 5]))  #
-print(len([1, 2, 3, 4, 5]))  # length of the list
-print(int(3.14))  # convert to integer
-print(float(10))  # convert to float
-print(str(123))  # convert to string
-print(bool(0))  # convert to boolean
-print(bool(1))  # convert to boolean    
-print(sqrt(16))  # square root
-print(log(100, 10))  # logarithm base 10
-print(log10(100))  # logarithm base 10
+# getting input from user
+name = input("What is your name? ") 
+# printing the output
+print("Hello, " + name + "!")
