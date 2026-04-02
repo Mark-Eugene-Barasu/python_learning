182b8a095f9e9cfd7a3f40cf5a09284064e3af3c
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-02 06:12:04 +0200

    a simple madlibs game, to ask user for inputs to print out a nice string

--- hello.py
+++ hello.py
@@ -1,14 +1,11 @@
-# basic calculator  
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-operation = input("Enter operation (+, -, *, /): ")
-if operation == '+':
-    result = num1 + num2
-elif operation == '-':  
-    result = num1 - num2
-elif operation == '*':
-    result = num1 * num2
-elif operation == '/':
-    result = num1 / num2
+# madlibs game
+print("Welcome to the Mad Libs Game!")
 
-print("Result:", result)
+color = input("Enter a color: ")
+flower = input("Enter a type of flower: ")
+adjective = input("Enter an adjective: ")
+
+print(f"Roses are {color},")
+print(f"Violets are {flower},")
+print(f"Sugar is {adjective},")
+print(f"And so are you!")
