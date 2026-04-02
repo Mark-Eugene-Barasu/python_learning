ff5763a390a416ec0f48ff6a70ab4afdee0ebbd9
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-02 05:45:24 +0200

    intentional mistake

--- hello.py
+++ hello.py
@@ -4,8 +4,5 @@ b = 3.14
 print(a)  # Output: 10
 print(b)  # Output: 3.14
 
-print(a + b)  # Output: 13.14
-print(a * b)  # Output: 31.400000000000002
-print(a / b)  # Output: 3.1847133757961785
-print(a // b) # Output: 3.0 (floor division)
-print(a % b)  # Output: 0.86 (modulus)
+print(a + "is my number")  # This will raise a TypeError because you cannot concatenate a string and an integer directly
+# To fix this, you can convert the integer to a string:
