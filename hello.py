69ae60b7b0f13efc06fb977047aaaa2747b277c2
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-02 05:59:14 +0200

    getting multiple inputs

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 # getting input from user
 name = input("What is your name? ") 
+age = input("How old are you? ")
 # printing the output
-print("Hello, " + name + "!")
+print("Hello, " + name + "! You are " + age + " years old.")
