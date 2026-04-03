77a2bcfa9fd47b6c7539c459448dab926a832ac2
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-03 07:49:17 +0200

    more string methods

--- hello.py
+++ hello.py
@@ -4,3 +4,7 @@ print(phrase.upper())
 print(phrase.isupper())
 print(phrase.upper().isupper())
 print(len(phrase))  
+print(phrase[0])  #H is the first character
+print(phrase[7])    #W is the 8th character (index starts at 0)
+print(phrase.index("X"))  #Find the index of 'X' (will raise an error if not found)
+print(phrase.replace("Hello", "Hi"))  #Replace "Hello" with "Hi"
