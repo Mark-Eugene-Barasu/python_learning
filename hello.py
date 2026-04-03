ff058e1caf0f28f348b229445eb3b8c24e67c659
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-03 08:25:52 +0200

    getting user inputs

--- hello.py
+++ hello.py
@@ -1,13 +1,5 @@
-# using math imports
-from math import *
-
-print(sqrt(abs(-16)))  # prints 4.0
-print(pi)        # prints 3.141592653589793
-print(floor(3.7))  # prints 3
-print(ceil(3.2))   # prints 4
-
-# using math with alias
-import math as m 
-print(m.sqrt(25))  # prints 5.0
-print(m.pi)        # prints 3.141592653589793
-
+# getting input from user
+name = input("What is your name? ")
+age = input("How old are you? ")
+# printing the output
+print("Hello, " + name + "! You are " + age + " years old.")
