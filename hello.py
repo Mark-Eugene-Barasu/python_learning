86b10ca10ab5482f791ebca6b5c4d352c5065665
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-03 09:00:48 +0200

    lists and tuples

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
-# creating a basic calculator using python
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-print("The sum of", num1, "and", num2, "is", num1 + num2)
+# list in python
+my_list = [1, 2, 3, 4, 5] # lists are mutable, meaning they can be changed after they are created
+print(my_list)
+
+# tuple in python
+my_tuple = (1, 2, 3, 4, 5) # tuples are immutable, meaning they cannot be changed after they are created
+print(my_tuple)
