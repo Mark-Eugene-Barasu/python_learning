987f393c51144c732a9e76a76b630c14f16c362d
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-03 08:06:42 +0200

    more practice on the previous committed versions of this file

--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
-
-my_number = 5
-print(my_number ** 2)
-print(pow(my_number, 2)) # both of these lines will print 25, which is 5 squared
-
-print(my_number ** 3) # 125, which is 5 cubed
-print(pow(my_number, 3)) # 125, which is 5 cubed
-
+my_numb = 10
+print(max(my_numb, 5)) # This will print the maximum of my_numb and 5, which is 10.
+print(min(my_numb, 5)) # This will print the minimum of my_numb and 5, which is 5.
+print(abs(-5)) # This will print the absolute value of -5, which is 5.
+print(pow(2, 3))    # This will print 2 raised to the power of 3, which is 8.
+print(round(3.14159, 2)) # This will print 3.14159 rounded to 2 decimal places, which is 3.14.
