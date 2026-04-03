57fc111ad524dee83d3535061f56e5eee3d8d78c
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-03 07:54:22 +0200

    numbers can be stored in variables

--- hello.py
+++ hello.py
@@ -1,8 +1,2 @@
-# working with numbers
-print(2 + 3)  # addition
-print(5 - 2)  # subtraction
-print(4 * 3)  # multiplication
-print(10 / 2)  # division
-print(10 // 3)  # floor division
-print(2 ** 3)  # exponentiation
-print(10 % 3)  # modulus
+my_num = 5
+print(my_num)
