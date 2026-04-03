fee65e01af563c29c0033249f615c6b0bd0c12ce
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-03 08:03:10 +0200

    using pow() function

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 
-my_number = 5.2
-print(abs(my_number)) # 5.2
-print(abs(-my_number)) # 5.2
-print(abs(-5)) # 5
-print(abs(5)) # 5
+my_number = 5
+print(my_number ** 2)
+print(pow(my_number, 2)) # both of these lines will print 25, which is 5 squared
+
+print(my_number ** 3) # 125, which is 5 cubed
+print(pow(my_number, 3)) # 125, which is 5 cubed
+
