43a4eea570dffeeccfdb4b14bdaa4c2fc0831aad
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-03 08:17:00 +0200

    using math import and using math with alias

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
-my_numb = 10
-print(max(my_numb, 5)) # This will print the maximum of my_numb and 5, which is 10.
-print(min(my_numb, 5)) # This will print the minimum of my_numb and 5, which is 5.
-print(abs(-5)) # This will print the absolute value of -5, which is 5.
-print(pow(2, 3))    # This will print 2 raised to the power of 3, which is 8.
-print(round(3.14159, 2)) # This will print 3.14159 rounded to 2 decimal places, which is 3.14.
+# using math imports
+from math import *
+
+print(sqrt(16))  # prints 4.0
+print(pi)        # prints 3.141592653589793
+print(floor(3.7))  # prints 3
+print(ceil(3.2))   # prints 4
+
+# using math with alias
+import math as m 
+print(m.sqrt(25))  # prints 5.0
+print(m.pi)        # prints 3.141592653589793
+
