3420aa474e59efdea7a2aff22fb2278b9ceb5480
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-03 07:41:12 +0200

    string methods

--- hello.py
+++ hello.py
@@ -1,12 +1,6 @@
-# madlibs game
-print("Welcome to the Mad Libs Game!")
-
-color = input("Enter a color: ")
-flower = input("Enter a type of flower: ")
-adjective = input("Enter an adjective: ")
-
-print(f"Roses are {color},")
-print(f"Violets are {flower},")
-print(f"Sugar is {adjective},")
-print(f"And so are you!")
-
+phrase = "Hello, World!"
+print(phrase.lower())
+print(phrase.upper()) 
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))  
