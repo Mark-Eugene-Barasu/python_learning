3feea177c1e0cf32475bfeab40853a044f40997c
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-05 11:10:12 +0200

    more math methods

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 import math
 pi = 3.14
+x = 10
+y = 20
+z = 32
 
 print(round(pi))
 print(math.ceil(pi))
@@ -7,3 +10,5 @@ print(math.floor(pi))
 print(abs(pi))
 print(pow(pi, 2))
 print(math.sqrt(pi))
+print(max(x, y, z))
+print(min(x, y, z))
