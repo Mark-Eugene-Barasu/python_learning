200eb8a169beb3a518e41b244cc9008503fb203b
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-05 12:17:13 +0200

    sets in python

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
-# tuple = a collection which is ordered and unchangeable used to group together related data
-student = ("Alice", 25, "Computer Science")
-print(student)  # prints ('Alice', 25, 'Computer Science')
-print(student[0])  # prints 'Alice'
-print(student[1])  # prints 25
-print(student[2])  # prints 'Computer Science'
-# student[0] = "Bob"  # This will raise an error because tuples are immutable
-# However, you can concatenate tuples to create a new tuple
-new_student = student + ("Senior",)
-print(new_student)  # prints ('Alice', 25, 'Computer Science', 'Senior')    
+# set = collection of unique items
+# set is unordered, so we cannot access items by index
+# sets are mutable, so we can add and remove items
+
+my_set = {1, 2, 3, 4, 5}
+print(my_set)
+
+for item in my_set:
+    print(item)
