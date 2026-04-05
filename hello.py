e04e73a244e56b31fc7305f690540f7ba69ee161
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-05 12:06:28 +0200

    2D list or multi dimentional lists

--- hello.py
+++ hello.py
@@ -1,33 +1,12 @@
-# list = used to store multiple items in a single variable
-# list is one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usages.
-# lists are created using square brackets: []
-# list items are ordered, changeable, and allow duplicate values.
-# list items are indexed, the first item has index [0], the second item has index [1] etc.
+# 2D list in python
+my_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(my_list)  # prints [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# Accessing elements in a 2D list
+print(my_list[0][0])  # prints 1
+print(my_list[1][2])  # prints 6    
 
-# create a list
-mylist = ["apple", "banana", "cherry"]
-print(mylist)
-# access list items
-print(mylist[0])  # prints "apple"
-print(mylist[1])  # prints "banana"
-print(mylist[2])  # prints "cherry"
-# change list items
-mylist[1] = "orange"
-print(mylist)  # prints ["apple", "orange", "cherry"]
-# add list items
-mylist.append("grape")
-print(mylist)  # prints ["apple", "orange", "cherry", "grape"]
-# remove list items
-mylist.remove("orange")
-print(mylist)  # prints ["apple", "cherry", "grape"]
-# loop through a list
-for item in mylist:
-    print(item)
-# check if an item is in a list
-if "apple" in mylist:
-    print("Yes, 'apple' is in the list")
-# get the length of a list
-print(len(mylist))  # prints 3
-# clear a list
-mylist.clear()
-print(mylist)  # prints []
+drinks = ["coffee", "tea", "juice"]
+dinner = ["pizza", "pasta", "salad"]
+dessert = ["cake", "ice cream", "pie"]
+food = [drinks, dinner, dessert]
+print(food)  # prints [['coffee', 'tea', 'juice'], ['pizza', 'pasta', 'salad'], ['cake', 'ice cream', 'pie']]
