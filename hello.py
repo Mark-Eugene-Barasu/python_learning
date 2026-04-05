cdd6f0c4314c877993ace9b1193f741671a63609
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-05 11:21:14 +0200

    slicing

--- hello.py
+++ hello.py
@@ -1,14 +1,17 @@
-import math
-pi = 3.14
-x = 10
-y = 20
-z = 32
 
-print(round(pi))
-print(math.ceil(pi))
-print(math.floor(pi))   
-print(abs(pi))
-print(pow(pi, 2))
-print(math.sqrt(pi))
-print(max(x, y, z))
-print(min(x, y, z))
+# slicing = creating a substring by extracting elements from another string
+#            indexing[] or slice()
+#            [start:stop:step]
+name = "Bro Code"
+first_name = name[0:3]
+last_name = name[4:8]
+funky_name = name[0:8:2]
+reversed_name = name[::-1]
+
+print(first_name)
+print(last_name)
+print(funky_name)
+print(reversed_name)
+website = "http://www.google.com"
+slice = slice(7, -4)
+print(website[slice])
