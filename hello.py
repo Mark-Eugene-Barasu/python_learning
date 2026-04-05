dd22020636dfceb205717d06a322ddc0936bad57
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-05 11:27:12 +0200

    more on if, elif and else

--- hello.py
+++ hello.py
@@ -1,17 +1,12 @@
 # if statement = a block of code that will execute if it's condition is true
-age = int(input("How old are you? "))
-if age >= 18:
-    print("You are an adult!")
-else:
-    print("You are a child!")
 
-
-# elif statement = a block of code that will execute if it's condition is true
-# and is used after if statement and before else statement      
 age = int(input("How old are you? "))
-if age >= 18:
+
+if age == 100:
+    print("You are a century old!")
+elif age >= 18:
     print("You are an adult!")
-elif age >= 13:
-    print("You are a teenager!")
+elif age < 0:
+    print("You haven't been born yet!")
 else:
     print("You are a child!")
