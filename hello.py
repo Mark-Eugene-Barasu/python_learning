f3e60772c283a93893f6641fe6585697d483021b
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-05 12:32:09 +0200

    using index [] in python

--- hello.py
+++ hello.py
@@ -1,16 +1,21 @@
-# dictionary = a changeable, unordered collection of unique key:value pairs
-capitals = {'USA': 'Washington DC',
-            'India': 'New Delhi',
-            'China': 'Beijing',
-            'Russia': 'Moscow'}
-print(capitals['Russia'])
-print(capitals.get('India'))
-print(capitals.get('Germany', 'Not Found'))
-capitals['Germany'] = 'Berlin'
-print(capitals)
-capitals.update({'Germany': 'Berlin'})
-print(capitals)
-capitals.pop('China')
-print(capitals)
-capitals.clear()
-print(capitals)
+# index operator [] = used to access a specific element in a collection using its index
+# string, list, tuple
+name = "Bro Code"
+first_letter = name[0]
+
+if(name[0].islower()):
+    print("First letter is lowercase")
+else:
+    print("First letter is uppercase")
+
+# name[0] = "B" # error, string is immutable
+# lists are mutable
+food = ["pizza", "hamburger", "hotdog", "spaghetti", "sushi"]
+food[0] = "taco"
+print(food[0]) # taco
+print(food[1]) # hamburger
+print(food[2]) # hotdog
+print(food[3]) # spaghetti
+print(food[4]) # sushi
+
+
